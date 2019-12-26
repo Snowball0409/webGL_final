cc9d14be8ba8ee08b433ce0c353f432d79c3fa32
Snowball0409 <55529968+Snowball0409@users.noreply.github.com>
2019-12-26 15:24:20 +0800

    Update vshader.js

--- vshader.js
+++ vshader.js
@@ -19,10 +19,10 @@ uniform float volume;
 
 void main()
 {
-	vec4 vertex = vPosition;
+    vec4 vertex = vPosition;
     vec4 L = normalize( lightPosition - vPosition ); // Light vector
     vec4 N = normalize( vNormal );	// Normal vector
-	vec4 V = normalize( eyePosition - vPosition );		// Eye vector.
+		vec4 V = normalize( eyePosition - vPosition );		// Eye vector.
     vec4 H = normalize( L + V );  // Halfway vector in the modified Phong model
 
 
@@ -32,13 +32,13 @@ void main()
     float Kd = max( dot(L, N), 0.0 );
     vec4  diffuse = Kd * materialDiffuse;
 
-	float Ks = pow( max(dot(N, H), 0.0), shininess );
+		float Ks = pow( max(dot(N, H), 0.0), shininess );
 	
     fColor = (ambient + diffuse) * vColor;
-	fTexCoord = vTexCoord;
+		fTexCoord = vTexCoord;
 	
 	// Scale the vertex with the audio volume
-	vertex.xyz *= 0.7 + 0.3 * volume;
+		vertex.xyz *= 0.7 + 0.3 * volume;
 
     gl_Position = projectionMatrix * viewingMatrix * modelingMatrix * vertex;
 }
